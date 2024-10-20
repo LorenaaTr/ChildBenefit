cf86b16d10a8bf3d912a39bc887cfc2c9404ea6f
Login Laptops <Login Laptops@DESKTOP-CGMLKBD>
2024-10-20 14:39:52 +0200

    Add project files.

---
+++ tema/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tema.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
