1f10e42a41a1bdbb6c87c0e734aa33db4df8568d
LorenaaTr <lorenatroshupa@gmail.com>
2024-10-21 15:28:24 +0200

    Created Gender tabel

---
+++ tema/Controllers/GenderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tema.Controllers
+{
+    public class GenderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ tema/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using tema.Models;
+
+namespace tema.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Gender> Genders { get; set; }
+    }
+}
