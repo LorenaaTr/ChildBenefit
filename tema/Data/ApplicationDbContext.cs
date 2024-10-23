e2c89ff776d8728007fd3888c30b6a819ccf0f72
lorenatroshupa <lorenatroshupa@gmail.com>
2024-10-23 15:24:14 +0200

    models

--- tema/Data/ApplicationDbContext.cs
+++ tema/Data/ApplicationDbContext.cs
@@ -11,5 +11,14 @@ namespace tema.Data
         }
 
         public DbSet<Gender> Genders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Gender>().HasData(
+                new Gender { Id = 1, AlDescription = "Femer", EnDescription = "Female", SrDescription = "Zensko" },
+                new Gender { Id = 2, AlDescription = "Mashkull", EnDescription = "Male", SrDescription = "Muski" },
+                new Gender { Id = 3, AlDescription = "Te tjera", EnDescription = "Others", SrDescription = "Drugi" }
+                );
+        }
     }
 }
